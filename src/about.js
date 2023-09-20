234acad3c515ccdd620eb7fc9162d8e2319d5ffc
Nurul Huda <nurulhuda5801@gmail.com>
2023-09-20 17:28:50 +0600

    hiii

---
+++ src/about.js
@@ -0,0 +1,11 @@
+
+// function about(){
+//     return(
+//        <div>
+//         <form action="" method="post">
+//             <input type="text" name="user" id="" />
+//             <input type="text" name="email" id="" />
+//         </form>
+//        </div> 
+//     );
+// }
